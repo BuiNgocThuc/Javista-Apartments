3ecbee2d149654f62df734bf5fb234bb12890273
Thuc Ngoc Bui <thucngb.dev@gmail.com>
2024-10-07 07:02:19 +0700

    creating initial project

---
+++ src/main/java/com/example/javista/JavistaApplication.java
@@ -0,0 +1,13 @@
+package com.example.javista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavistaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavistaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javista/JavistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavistaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
